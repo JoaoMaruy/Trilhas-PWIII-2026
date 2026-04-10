8742a858af9504e59430f531baeeb3ec261bfb13
João Maruyama <Joao.Maruyama@alunos.ifsuldeminas.edu.br>
2026-04-10 14:20:27 -0300

    Sitema de login (login.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const loginRoutes = require("./src/routes/loginRoutes");
-
 const app = express();
 const PORT = 3000;
-
-//Middleware
 app.use(cors());
 app.use(express.json());
-
-//Routes
 app.use("/api/login", loginRoutes);
-
 app.listen(3000, () => {
-   console.log("Servidor rodando na porta 3000");
-})
+  console.log("Servidor rodando na porta 3000");
+});
